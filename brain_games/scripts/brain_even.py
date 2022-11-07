11ac7ed666b377655eea0e9a852b05c9048f8df5
edvin3i <edviniii@gmail.com>
2022-11-07 11:02:52 +0500

    some problems with importing

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,47 @@
 #!/usr/bin/env python3
 
-from brain_games import game_even
+import prompt
+from random import randint
+
+
+def welcome_user():
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print(f'Answer "yes" if the number is even, otherwise answer "no".')
+    return name
+
+
+def is_even(num):
+    if num % 2:
+        return 'no'
+    return 'yes'
+
+
+def ask_question(num):
+    print(f'Question: {num}')
+    answer = prompt.string(f'Your answer: ')
+    return answer
+
+
+def start_game():
+    name = welcome_user()
+    try_num = 1
+
+    while try_num <= 3:
+        num = randint(1, 100)
+        user_answer = ask_question(num).lower()
+        right_answer = is_even(num)
+        if right_answer == user_answer:
+            print('Correct!')
+        else:
+            print(f"""'{user_answer}' is wrong answer ;(. Correct answer was '{right_answer}'.\nLet's try again, {name}!""")
+            return
+        try_num += 1
+    print(f'Congratulations, {name}!')
 
 
 def main():
-    game_even.start_game()
+    start_game()
 
 
 if __name__ == '__main__':
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-from brain_games import cli
+#from brain_games import cli
+from brain_games import game_even
 
 
 def main():
